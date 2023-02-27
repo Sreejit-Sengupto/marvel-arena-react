2d84131bdf7f9baedf6a12bb30261c09e0f13fa6
Sreejit <sreesen2003@gmail.com>
2023-02-27 22:34:41 +0530

    Adds transition to description section

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './styles/card.css'
 
+const mountedStyle = {
+    animation: "inAnimation 500ms ease-in"
+};
+const unmountedStyle = {
+    animation: "outAnimation 500ms ease-out",
+    animationFillMode: "forwards"
+}
 export default function Card (props) {
     const trailer = (props.content.trailer) === "" ? "Yet to be released" : <p className="trailer"><a href={props.content.trailer} target="_blank" rel="noreferrer" className="trailerLink">Watch the
     Teaser Trailer here!</a></p>
 
+    const [isMounted, setIsMounted] = React.useState(false);
     const [render, setRender] = React.useState(false);
+
     function handleChange() {
+        setIsMounted(prevState => !prevState);
         setRender(prevState => !prevState);
     }
 
@@ -17,7 +27,11 @@ export default function Card (props) {
                 <button onClick={handleChange} className="btn" >{!render ? "Read" : "Hide"}</button>
             </div>
 
-            {render && <div className="content">
+            {render && <div className="content" 
+                style={isMounted ? mountedStyle : unmountedStyle}
+                onAnimationEnd={() => {
+                  if (!isMounted) setRender(false);}}
+            >
                 <p>{props.content.description}</p>
                 <p className="twitter">
                     <a href={props.content.twitterLink} className="twitterLink" target="_blank" rel="noreferrer">Follow Captain America on Twitter</a>

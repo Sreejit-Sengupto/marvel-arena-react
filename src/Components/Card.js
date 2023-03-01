0fe1a31a44ea597967c0758ba952937d6f59121e
Sreejit <sreesen2003@gmail.com>
2023-03-01 18:24:12 +0530

    Added animations

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,43 +1,35 @@
 import React from "react";
 import './styles/card.css'
+import { motion } from 'framer-motion';
 
-const mountedStyle = {
-    animation: "inAnimation 500ms ease-in"
-};
-const unmountedStyle = {
-    animation: "outAnimation 500ms ease-out",
-    animationFillMode: "forwards"
-}
 export default function Card (props) {
     const trailer = (props.content.trailer) === "" ? "Yet to be released" : <p className="trailer"><a href={props.content.trailer} target="_blank" rel="noreferrer" className="trailerLink">Watch the
     Teaser Trailer here!</a></p>
 
-    const [isMounted, setIsMounted] = React.useState(false);
     const [render, setRender] = React.useState(false);
 
     function handleChange() {
-        setIsMounted(prevState => !prevState);
         setRender(prevState => !prevState);
     }
 
     return (
-        <div className="updateStyle">
-            <div className="img--container">
-                <img src={props.content.image} alt="captain america new world order" className="img" />
+        <motion.div className="updateStyle" initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{delay: 0.18}}>
+            <motion.div className="img--container" whileHover={{scale: 1.05}}>
+                <img src={props.content.image} alt="banner" className="img" />
                 <button onClick={handleChange} className="btn" >{!render ? "Read" : "Hide"}</button>
-            </div>
+            </motion.div>
 
-            {render && <div className="content" 
-                style={isMounted ? mountedStyle : unmountedStyle}
-                onAnimationEnd={() => {
-                  if (!isMounted) setRender(false);}}
+            {render && <motion.div className="content"
+                animate={{scale: 1}}
+                initial={{scale: 0}}
+                transition={{delay: 0.1}}
             >
                 <p>{props.content.description}</p>
                 <p className="twitter">
                     <a href={props.content.twitterLink} className="twitterLink" target="_blank" rel="noreferrer">Follow Captain America on Twitter</a>
                 </p>
                 {trailer}
-            </div>}
-    </div>
+            </motion.div>}
+    </motion.div>
     )
 }
